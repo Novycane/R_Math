c1ca689beac67f58a31b02a4b1c637f49d61c9a6
Steven Novak <stevenrnovak@gmail.com>
2017-01-25 15:00:31 -0800

    Added Euler's Method for soling first order differential equations

---
+++ FiniteDiff.R
@@ -0,0 +1,35 @@
+# FiniteDiff.R
+#
+# Routines For Calculating solutions to differential equations
+#
+# 01/25/2017
+#
+#
+# Steven Novak
+#
+
+# --------------------------------------------- Euler Routines
+
+# Eulers Method for first order differential equations
+#
+# FUN a function in the form dY/dx = FUN(x)
+# a is start of range
+# b is the end of the range
+# step is the step size
+#
+
+Euler <- function(FUN, a, b, step, ic)
+{
+	y <- vector("numeric", (b-a) / step)
+
+	i = 1
+	y[i] <- ic
+	
+	for(x in seq(a + step,b,step))
+	{
+		i = i + 1
+		y[i] <- y[i-1] + step * FUN(y[i-1])
+	}
+
+	return(y)
+}
